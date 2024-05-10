bd5f237da8a53d4a6f52fd8cd05b470e9be80aad
Meiryanne Martins Pereira <meirymartinsp.rodrigues@gmail.com>
2024-05-10 16:23:23 -0300

    feat: add on menu modal for register new wine

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import "./App.css";
 import { Card } from './components/card/card';
 import { useMenuData } from './hooks/useMenuData';
+import { CreateModal } from './components/create-modal/create-modal';
 
 function App() {
   const { data } = useMenuData();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () =>{
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
     <>
-    <div className="conteiner">
+    <div className="container">
       <h1>Menu</h1>
       <div className="card-grid">
         {data?.map(wineData =>
@@ -18,6 +25,8 @@ function App() {
           />
         )}
       </div>
+      {isModalOpen && <CreateModal />}
+      <button onClick={handleOpenModal}>novo</button>
     </div>
     <footer className="rodape">
       <div>
